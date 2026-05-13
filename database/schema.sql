21dd94b24e6775671145677051095eed69931db4
Nourelden Hany Muhammed <nour.hany.elhakiem@gmail.com>
2026-05-14 02:04:08 +0300

    db: initialize schema file with reset statements

---
+++ database/schema.sql
@@ -0,0 +1,17 @@
+-- =========================================================
+-- Freelance Marketplace Management System
+-- Database Schema
+-- =========================================================
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS Freelancer_Skill;
+DROP TABLE IF EXISTS Contract;
+DROP TABLE IF EXISTS Proposal;
+DROP TABLE IF EXISTS Project;
+DROP TABLE IF EXISTS Skill;
+DROP TABLE IF EXISTS Freelancer;
+DROP TABLE IF EXISTS Client;
+DROP TABLE IF EXISTS `User`;
+
+SET FOREIGN_KEY_CHECKS = 1;

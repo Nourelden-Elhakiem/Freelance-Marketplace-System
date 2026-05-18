2129db7f8274de9cae65dc521486701938966bd6
Nourelden Hany Muhammed <nour.hany.elhakiem@gmail.com>
2026-05-18 04:10:30 +0300

    feat: add about page route shell

---
+++ pages/about.php
@@ -0,0 +1,6 @@
+﻿<?php
+$pageTitle = 'About | Freelance Marketplace System';
+include __DIR__ . '/../includes/header.php';
+include __DIR__ . '/../includes/navbar.php';
+?>
+<?php include __DIR__ . '/../includes/footer.php'; ?>
